9e482727e447530b482f2c30d5a8678391806dd2
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-03-19 12:16:17 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,6 @@
-// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-// 🔹 Apna Supabase URL aur Anon Key yaha paste kare
-const SUPABASE_URL = "https://zbwduubkneapofdeyvro.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpid2R1dWJrbmVhcG9mZGV5dnJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4ODg1NjgsImV4cCI6MjA4OTQ2NDU2OH0.bcfvnzG8ZhuGeJn3zvK5h6sfftXVZmlYwEknojSuPE4";
-
-// 🔹 Client create
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  "https://zbwduubkneapofdeyvro.supabase.co",
+  "sb_publishable_55O0SzvEeXQxNSd_TGrwbg_HSGgLMYu"
+);
